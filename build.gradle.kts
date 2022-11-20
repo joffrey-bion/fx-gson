a7665c422af98652b7c90bf68437516234dd0326
Joffrey Bion <joffrey.bion@gmail.com>
2022-11-20 21:38:50 +0100

    Upgrade JetBrains annotations to 23.0.0
    Resolves:
    https://github.com/joffrey-bion/fx-gson/issues/26

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ val checkstyleConfig by configurations.creating {}
 
 dependencies {
     api("com.google.code.gson:gson:2.10")
-    compileOnlyApi("org.jetbrains:annotations:18.0.0")
+    compileOnlyApi("org.jetbrains:annotations:23.0.0")
     testImplementation("junit:junit:4.+")
     checkstyleConfig("org.hildan.checkstyle:checkstyle-config:2.5.0")
 }
